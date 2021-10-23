28c925b0df75f192f5a7d9324e0916d923318a62
haideralrustem <haideralrustem@yahoo.com>
2021-10-23 04:24:06 -0500

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,23 @@ function Home() {
             
             <div class='home-background-img'>
 
-                <h1 class='home-title crimson-font'>Welcome To EVEntor</h1>
-                <h2 class='home-subtitle crimson-font'>Find events near you!!</h2>
+                <div className='home-welcome-title-wrap'>
+                    <h1 class='home-title crimson-font'>Welcome To EVEntor</h1>
+                    <h2 class='home-subtitle crimson-font'>Find events near you!!</h2>
+                </div>
+                
 
-                <div>
-                    <h3 class="crimson-font">Start by entering your in the box below ... </h3>
+                <div className='home-search-wrap'>
+                    <h3 className="home-search-subtitle crimson-font">Start by entering your in the box below ... </h3>
                     <div>
                         <form>
-                            <input type='text' class='home-search-box'  
-                                id='home-search-box'></input>
-                            <button class="home-search-button crimson-font">Find Events</button>
+                            <div className='home-input-button-wrap'>
+                                <input type='text' class='home-search-box'  
+                                    id='home-search-box'></input>
+                                <button class="home-search-button crimson-font">Find Events</button>
+                            </div>
+
+                            
                         </form>
                     </div>
                 </div>
